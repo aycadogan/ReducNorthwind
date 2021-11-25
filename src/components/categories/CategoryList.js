c8edb341f9617b5d9dd672724423c5214ddc593a
aycadogan89 <doganayca89@gmail.com>
2021-11-24 23:26:39 -0800

    redux thunk

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {connect} from "react-redux"
+import {bindActionCreators} from "redux"
+import * as categoryActions from "../../redux/actions/categoryActions"
+import {ListGroup, ListGroupItem} from "reactstrap"
+
+
+class CategoryList extends Component {
+
+    componentDidMount(){this.props.actions.getCategories()}
+    render() {
+        return (
+            <div>
+                <h3>Categories </h3>
+                <ListGroup>
+                    {
+                        this.props.categories.map(category => (
+                            <ListGroupItem key={category.id}>
+                                {category.categoryName}
+                            </ListGroupItem>
+                        ))
+                    }
+                    
+                </ListGroup>
+                <h5>Seçili Category: {this.props.currentCategory.categoryName}</h5>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        currentCategory: state.changeCategoryReducer,
+        categories: state.categoryListReducer
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return{
+        actions:{
+            getCategories:bindActionCreators(categoryActions.getCategories,dispatch)
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CategoryList)
---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,14 @@
+export default {
+    currentCategory: 
+        {categoryName: "Beverages"},
+    categories:[ 
+        { categoryName:"Beverages"},
+        {categoryName: "Condiments"},
+        {categoryName: "Confections"},
+        {categoryName: "Dairy Products"},
+        {categoryName: "Grains/Cereals"},
+        {categoryName: "Meat/Poultry"},
+        {categoryName: "Produce"},
+        {categoryName: "Seafood"}
+    ]
+}
